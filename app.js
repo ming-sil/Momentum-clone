fc6326846490945a854bc9b599767e24ab1c312a
ming-sil <choming4926@gmail.com>
2022-08-19 23:48:11 +0900

    Form Submission

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const loginInput = document.querySelector(".login-form input");
 const loginButton = document.querySelector(".login-form button");
 
 const onLoginBtnClick = () => {
-  console.log(loginInput.value);
-  console.log("click");
+//  const username = loginInput.value;
+//   if (username === "") {
+//     alert("Please write your name.");
+//   } else if (username.length > 15) {
+//     alert("Your name is too long.");
+//   }
 };
 
 loginButton.addEventListener("click", onLoginBtnClick);
