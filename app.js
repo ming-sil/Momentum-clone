216e1003200f82fd852197b99051f23659d6921a
ming-sil <choming4926@gmail.com>
2022-08-20 17:56:55 +0900

    Saving Username

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const onLoginSubmit = (event) => {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  console.log(username);
+  localStorage.setItem("username", username);
   greeting.innerText = `Hello ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 };
