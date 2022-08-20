01a735447e4f9fd9b88bef21c656a33f12292b0e
ming-sil <choming4926@gmail.com>
2022-08-20 14:51:54 +0900

    Events

--- app.js
+++ app.js
@@ -2,16 +2,19 @@
 // const loginInput = loginForm.querySelector("input")
 // const loginButton = loginForm.querySelector("button")
 
+const loginForm = document.querySelector(".login-form");
 const loginInput = document.querySelector(".login-form input");
-const loginButton = document.querySelector(".login-form button");
 
-const onLoginBtnClick = () => {
-//  const username = loginInput.value;
-//   if (username === "") {
-//     alert("Please write your name.");
-//   } else if (username.length > 15) {
-//     alert("Your name is too long.");
-//   }
+const onLoginSubmit = (event) => {
+  // const username = loginInput.value;
+  //   if (username === "") {
+  //     alert("Please write your name.");
+  //   } else if (username.length > 15) {
+  //     alert("Your name is too long.");
+  //   }
+
+  event.preventDefault();
+  console.log(loginInput.value);
 };
 
-loginButton.addEventListener("click", onLoginBtnClick);
+loginForm.addEventListener("submit", onLoginSubmit);
