c57a64545119da9e44d8d5c40b0577a10df60cfc
Min <106130309+ming-sil@users.noreply.github.com>
2022-08-27 16:28:41 +0900

    Deleting To Dos

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,21 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const deleteToDo = (e) => {
+    // console.log(e.target.parentElement.innerText);
+    const li = e.target.parentElement;
+    li.remove();
+}
+
 const paintToDo = (newToDo) => {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newToDo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
 }
 
