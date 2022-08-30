f4b5c24f0b419fdcf15ce712bf1b163cdcda8f4c
ming-sil <choming4926@gmail.com>
2022-08-31 01:03:14 +0900

    -

--- js/weather.js
+++ js/weather.js
@@ -3,7 +3,6 @@ const API_KEY = "5fa74294bfe4df1ad46db1a995011170";
 const onGeoOk = (position) => {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  console.log("You Live in", lat, lon);
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
