5812e5156b6aa7770cdf34c893265c169dbdca8a
ming-sil <choming4926@gmail.com>
2022-08-30 13:19:43 +0900

    Weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,18 @@
+const API_KEY = "5fa74294bfe4df1ad46db1a995011170";
+
 const onGeoOk = (position) => {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You Live in", lat, lng);
+  const lon = position.coords.longitude;
+  console.log("You Live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:last-child");
+      city.innerHTML = data.name;
+      weather.innerHTML = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 };
 
 const onGeoError = () => {
